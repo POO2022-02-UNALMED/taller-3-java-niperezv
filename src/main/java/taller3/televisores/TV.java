834945ffce6123261d9475e92d53c3d42c916295
niperezv <47955066+niperezv@users.noreply.github.com>
2022-08-29 16:12:11 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -2,103 +2,103 @@ package taller3.televisores;
 
 public class TV {
 	private Marca marca;
-	private int canal = 1;
-	private int precio = 500;
+	private int canal=1;
+	private int precio=500;
 	private boolean estado;
-	private int volumen = 1;
+	private int volumen=1;
 	private Control control;
 	private static int numTV;
+	
 	public TV(Marca marca, boolean estado) {
-		this.marca = marca;
-		this.estado = estado;
+		this.marca=marca;
+		this.estado=estado;
 		numTV++;
 	}
-	//Get_Set_marca
+	
+	public void setMarca(Marca marca) {
+		this.marca=marca;
+	}
+	
 	public Marca getMarca() {
 		return marca;
 	}
-	//¿que pasa si no se le pasa paramatro?
-	public void setMarca(Marca marca) {
-		this.marca = marca;
+	
+	public void setControl(Control control) {
+		this.control=control;
 	}
-	//Get_Set_precio
+	
+	public Control getControl() {
+		return control;
+	}
+	
+	public void setPrecio(int precio) {
+		this.precio=precio;
+	}
+	
 	public int getPrecio() {
 		return precio;
 	}
-	public void setPrecio(int precio) {
-		this.precio = precio;
+	
+	public void setVolumen(int volumen) {
+		this.volumen=volumen;
 	}
-	//Get_Set_Volumen
+	
 	public int getVolumen() {
 		return volumen;
 	}
-	public void setVolumen(int volumen) {
-		this.volumen = volumen;
-	}
-	//Get_Set_Canal
-	public int getCanal() {
-		return canal;
-	}
+	
 	public void setCanal(int canal) {
 		if(estado==true && (canal>=1 && canal<=120)) {
 			this.canal=canal;
 		}
+		
 	}
-	//Get_NumTV
-	public static int getNumTV() {
-		return numTV;
+	
+	public int getCanal() {
+		return canal;
 	}
-	//Get_Set_control
-	public Control getControl() {
-		return control;
-	}
-	public void setControl(Control control) {
-		this.control = control;
-	}
-	//turnOn_turnOff
-	void turnOn (boolean estado) {
-		if(estado == false)
-			estado=true;
-	}
-	void turnOff (boolean estado) {
-		if(estado == true)
-			estado =false;
-	}
-	//Get_Estado
-	public boolean getEstado() {
-		return estado;
-	}
+	
 	public static void setNumTV(int numTV) {
 		TV.numTV=numTV;
 	}
+	
 	public static int getNumTV() {
 		return numTV;
 	}
-	public void turnOn() {
+	
+	public void turnOn(){
 		estado=true;
 	}
-	public void turnOff() {
+	
+	public void turnOff(){
 		estado=false;
 	}
-	public void canalUp() {
+	
+	public boolean getEstado() {
+		return estado;
+	}
+	
+	public void canalUp(){
 		if(estado==true && canal<120) {
 			canal++;
 		}
 	}
-	public void canalDown() {
+	
+	public void canalDown(){
 		if(estado==true && canal>1) {
 			canal--;
 		}
 	}
-	public void volumenUp() {
+	
+	public void volumenUp(){
 		if(estado==true && volumen<7) {
 			volumen++;
 		}
 	}
-	public void volumenDown() {
+	
+	public void volumenDown(){
 		if(estado==true && volumen>0) {
 			volumen--;
 		}
 	}
-
 }
